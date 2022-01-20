d685fe7f3966926fff0ac46f217cfe1893bba013
Andreas Plank <andreas.plank@web.de>
2022-01-20 17:30:32 +0100

    Update fixRDF_before_validateRDFs.sh
    added fixes, rdf-header comparison, fix of squaredbrackets, add datatype to <dcterms:decimalLatitude> or <dcterms:decimalLatitude>

--- bin/fixRDF_before_validateRDFs.sh
+++ bin/fixRDF_before_validateRDFs.sh
@@ -29,12 +29,12 @@ function usage() {
 function processinfo () {
 echo     "############ Fix RDF before validateRDF.sh #################"
 echo -e  "# Process for search pattern: \e[32m$file_search_pattern\e[0m ..."
-echo -e  "# Originals are kept as:      \e[32m${file_search_pattern}.$bak\e[0m ..." # final line break
+echo -e  "# Originals are kept as:      \e[32m${file_search_pattern}.$bak.gz\e[0m ..." # final line break
 echo -e  "# Read directory:  \e[32m${this_wd}\e[0m ..."
 if [ ${n} -gt 0 ];then
 echo -ne "# Do you want to process \e[32m${n}\e[0m files with search pattern: «\e[32m${file_search_pattern}\e[0m» ?\n# [\e[32myes\e[0m or \e[31mno\e[0m (default: no)]: \e[0m"
 else 
-echo -ne "# \e[0mBy using search pattern: «\e[32m${file_search_pattern}\e[0m» the number of processed files is \e[31m${n}\e[0m.\n# \e[31m(Stop) We stop here; please check the search pattern, directory and/or data.\e[0m\n";
+echo -ne "# \e[0mBy using search pattern: «\e[32m${file_search_pattern}\e[0m» the number of processed files is \e[31m${n}\e[0m.\n# \e[31m(Stop) We stop here; please check or add the correct search pattern, directory and/or data.\e[0m\n";
 exit 1;
 fi
 
@@ -94,29 +94,17 @@ echo "# Read passed …"
 
 for this_file in `ls $file_search_pattern | sort --version-sort`; do
 printf "# Process %03d of %03d in %s " $i $n "${this_file##*/}";
-#   sed --regexp-extended  '0,/<rdf:RDF/!{ /<rdf:RDF/{:label_rdfRDF; N; /[^>]*>/!b label_rdfRDF; s@<rdf:RDF[^>]*>@<!-- rdf:RDF replaced -->@;} }' Test_sed.rdf # failed
-#   sed --regexp-extended  '0,/<rdf:RDF/!{ /<rdf:RDF/N;s@<rdf:RDF[^>]*>@<!-- rdf:RDF replaced -->@}' Test_sed.rdf # failed because of prepended <!xml and not clear b pattt
-#   sed --regexp-extended  '0,/<\?xml/!{s@(<\?xml[^>]+>)@@};' # OK
-#   sed --regexp-extended  '0,/<rdf:RDF/!{ /<rdf:RDF/{:label_rdfRDF; N; /<rdf:RDF[^>]*>/!b label_rdfRDF; s@<rdf:RDF[^>]*>@<!-- rdf:RDF replaced -->@;} }' Test_sed.rdf # OK
-#   sed --regexp-extended  '0,/<\?xml/!{s@(<\?xml[^>]+>)@@}; 0,/<rdf:RDF/!{ /<rdf:RDF/{:label_rdfRDF; N; /<rdf:RDF[^>]*>/!b label_rdfRDF; s@<rdf:RDF[^>]*>@<!-- rdf:RDF replaced -->@;} }' Test_sed.rdf # OK
-#   sed --regexp-extended  '1 s@(.+)(<\?xml[^>]+>)@\2\1@;' Test_sed.rdf # 
-#   sed --regexp-extended  '0,/<!--/{N; s@(.+)(<\?xml[^>]+>)@\2\1@; }' Test_sed.rdf # OK
-#   sed --regexp-extended  '0,/<!--/{N; s@(.+)(<\?xml[^>]+>)@\2\1@; }; 0,/<\?xml/!{s@(<\?xml[^>]+>)@<!-- xml replaced -->@}; 0,/<rdf:RDF/!{ /<rdf:RDF/{:label_rdfRDF; N; /<rdf:RDF[^>]*>/!b label_rdfRDF; s@<rdf:RDF[^>]*>@<!-- rdf:RDF replaced -->@;} }' Test_sed.rdf #
-#   sed --regexp-extended  '/"https?:\/\/[^"]+\s+[^"]*"/ { :label_url_with_space; s@"(https?://[^" ]+)\s@"\1%20@; tlabel_url_with_space; }' Test_sed.rdf # 
-#### get RDF head
-#   sed -n '/<rdf:RDF/,/>/{ s@\bxmlns:@\nxmlns:@g; /\nxmlns:/!d; /^[\s\t\n]*$/d; p; }' Thread-1_data.nhm.ac.uk_20201111-1335.rdf | sort --unique | sed '1i\<rdf:RDF 
-# $a\ >'
 
-echo "#   extract all <rdf:RDF …> to ${this_file%.*}_rdfRDF_head.rdf ... " 
-  sed --regexp-extended --quiet '/<rdf:RDF/,/>/{ s@<rdf:RDF +@@; s@\bxmlns:@\nxmlns:@g; /\nxmlns:/!d; /^[\s\t\n]*$/d; p; }' "$this_file" | sort --unique \
+echo "#   extract all <rdf:RDF …> to ${this_file%.*}_rdfRDF_headers_extracted.rdf ... " 
+sed --regexp-extended --quiet '/<rdf:RDF/,/>/{ s@<rdf:RDF +@@; s@\bxmlns:@\n  xmlns:@g; s@>@@; /\n  xmlns:/!d; /^[\s\t\n]*$/d; p; }' "$this_file" | sort --unique \
   | sed --regexp-extended --quiet  '/^[\t ]+$/d;/^$/d;p;' \
-  | sed '1i\<rdf:RDF 
-$a\ >' > "${this_file%.*}_rdfRDF_head.rdf"
+  | sed "1i\<rdf:RDF
+  \$a\>\n<\!-- these are all RDF-headers extracted from ${this_file} -->" > "${this_file%.*}_rdfRDF_headers_extracted.rdf"
   
   echo "#   fix xml head, rdf:RDF, illegal characters in URIs, '--' in comments etc.) ... " 
   sed --regexp-extended -i.$bak '
   0,/<!--/{N; s@(.+)(<\?xml[^>]+>)@\2\1@; };
-  # move comments after first starting <?xml…>
+  # move comments that may be there before first starting <?xml…>
   
   0,/<\?xml/!{s@(<\?xml[^>]+>)@<!-- xml replaced -->@}; 
   # replace all <?xml…> but the first
@@ -124,25 +112,37 @@ $a\ >' > "${this_file%.*}_rdfRDF_head.rdf"
   0,/<rdf:RDF/!{ /<rdf:RDF/{:label_rdfRDF; N; /<rdf:RDF[^>]*>/!b label_rdfRDF; s@<rdf:RDF[^>]*>@<!-- rdf:RDF replaced -->@;} }
   # replace all <rdf:RDF…> but the first
   
-  s@</rdf:RDF>@@;$ a\ </rdf:RDF>
-  # replace all </rdf:RDF…> but append at last
+  s@</rdf:RDF>@@; $ a\ </rdf:RDF>
+  # replace all </rdf:RDF…> but append at the very last line
   
   /<!--.*[^<][^!]--[^>].*-->/ { # rdfparse Fatal Error:  (line 75 column 113): The string "--" is not permitted within comments.
       :label.uriminus_in_comment; s@\s(https?://.+)--([^>]* -->)@ \1%2D%2D\2@; tlabel.uriminus_in_comment;
     }
   # replace all double minus -- in comments 
   
-  /"https?:\/\/[^"]+[ `\\]+[^"]*"/ { # replace characters that are not allowed in URL
+  # fix some characters that should be encoded (see https://www.ietf.org/rfc/rfc3986.txt)
+  /"https?:\/\/[^"]+[][ `\\]+[^"]*"/ { # replace characters that are not allowed in URL
       :label.urispace; s@"(https?://[^" ]+)\s@"\1%20@; tlabel.urispace;
       :label.uriaccentgrave; s@"(https?://[^"`]+)`@"\1%60@; tlabel.uriaccentgrave;
       :label.backslash; s@"(https?://[^"\\]+)\\@"\1%5C@; tlabel.backslash;
+      :label.leftsquaredbracket; s@"(https?://[^"\[]+)\[@"\1%5B@; tlabel.leftsquaredbracket;
+      :label.rightsquaredbracket; s@"(https?://[^"\[]+)\]@"\1%5D@; tlabel.rightsquaredbracket;
     }
+  # add datatype to <dcterms:decimalLatitude> or <dcterms:decimalLatitude>
+  s@(<)([[:alpha:]]+:)(decimalLongitude|decimalLatitude)(>)([^<>]*)(</\2\3>)@\1\2\3 rdf:datatype="http://www.w3.org/2001/XMLSchema#decimal"\4\5\6@g;
 
+  # remove double http… (some JACQ had such data)
+  s@(rdf:resource|rdf:about)="(https?://[^"]+)\2"@\1="\2"@
+  s@<(dwc:materialSampleID)>(https?://[^<>]+)\2</\1>@<\1>\2</\1>@
+  
   ' "$this_file" #
+  gzip --verbose "$this_file.$bak"
   i=$((i + 1))
-#     echo "#   Fix $this_file ($n_URI_spaces URIs with spaces) ... " && sed --regexp-extended -i.bak ' /"https?:\/\/[^"]+\s+[^"]*"/ { :label_url_with_space; s@"(https?://[^" ]+)\s@"\1%20@; tlabel_url_with_space; } ' "$this_file"
 done
-
-echo     "# Done." # final line break
-echo     "# Check also <rdf:RDF…> in ${file_search_pattern%.*}_rdfRDF_head.rdf ..." # final line break
-echo     "# Originals are kept in ${file_search_pattern}.$bak ..." # final line break
+echo     "# Done. Original data are kept in ${file_search_pattern}.$bak.gz ..." # final line break
+echo     "# Each RDF file is prepared for validation and could be imported from this point on." # final line break
+echo     "# Check also if the RDF-head is equal to the extracted ones with ${this_file%.*}_rdfRDF_headers_extracted.rdf ..." # final line break
+echo     "# You can use command pr to print the RDF headers side by side:" # final line break
+for this_file in `ls $file_search_pattern | sort --version-sort`; do
+echo     "#   f='${this_file}'; sed --quiet -r '/<rdf:RDF/,/>/{s@[ \t\s]+(xmlns:)@\n  \1@g; p}' \${f} | pr --page-width 140 --merge --omit-header \${f%.*}_rdfRDF_headers_extracted.rdf -" # final line break
+done
