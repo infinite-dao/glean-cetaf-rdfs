9c105c9aca91863625c89089a33a698c63afc3c3
Andreas Plank <andreas.plank@web.de>
2022-06-16 11:14:49 +0200

    Update import_rdf2trig.gz4docker-fuseki-app.sh
    add check to run inside docker else exit

--- bin/import_rdf2trig.gz4docker-fuseki-app.sh
+++ bin/import_rdf2trig.gz4docker-fuseki-app.sh
@@ -9,9 +9,18 @@
 # dependencies: awk
 # Usage example: /import-data/bin/import_rdf2trig.gz4docker-fuseki-app.sh -d 'xxx.jacq.org' -s 'Thread-*20211117-1006.rdf._normalized.ttl.trig.gz'
 
-sparql_end_point="CETAF-IDs"
-datetime_now=$(date '+%Y%m%d-%H%M%Ss')
-DOMAINNAME='id.snsb.info'
+if grep -q docker /proc/1/cgroup; then 
+    echo -e  "# \e[32mOK\e[0m inside docker "
+else
+   echo -e  "# \e[31mError:\e[0m script is executet on host but needs to run inside docker (stop)."
+   echo -e  "#        You can enter the docker container by «docker exec -it containername bash», e.g. docker exec -it fuseki-app bash"
+   exit
+fi
+
+SPARQL_END_POINT_DATASET="CETAF-IDs"
+datetime_now=$(date '+%Y%m%d-%H%M%S')
+URN_DOMAINNAME='id.snsb.info'
+GRAPH='default'
 
 # LOGFILE="Thread-import-trig_data.biodiversitydata.nl_0000001-7963204${datetime_now}.log"
 # FILE_SEARCH_PATTERN="Thread-*_coldb.mnhn.fr_*2020-06-*.rdf._normalized.ttl.trig.gz"
@@ -47,24 +56,25 @@ THIS_WD="/import-data/rdf/tmpimport-jacq_20211206"
 FILE_SEARCH_PATTERN="Thread-*finnland.fi_20211206-1647.rdf._normalized.ttl.trig.gz"
 LOGFILE="Import_Thread-finland.fi_${datetime_now}.log"
 THIS_WD="/import-data/rdf/Finnland"
-DOMAINNAME='finnland.fi'
+URN_DOMAINNAME='finnland.fi'
 
 THIS_WD="${PWD}"
 
-LOGFILE="Import_Thread-jacq.org_${datetime_now}.log"
-DOMAINNAME='jacq.org'
 
 LOGFILE="Import_inside_Thread-XX-data.rbge.org.uk_20220228ff_${datetime_now}.log"
-DOMAINNAME='data.rbge.org.uk'
+URN_DOMAINNAME='data.rbge.org.uk'
+
+LOGFILE="Import_Thread-jacq.org_${datetime_now}.log"
+URN_DOMAINNAME='jacq.org'
 # LOGFILE=""
-# DOMAINNAME=""
+# URN_DOMAINNAME=""
 
 i=1; 
 # set (i)ndex and (n)umber of files alltogether
 
 
 function reset_logfile_name () {
-  LOGFILE_DEFAULT=`printf "Import_fuseki_%s_${DOMAINNAME}_${datetime_now}.log" X`
+  LOGFILE_DEFAULT=`printf "Import_fuseki_%s_${URN_DOMAINNAME}_${datetime_now}.log" X`
   if [[ -z "${LOGFILE// /}"  ]];then LOGFILE=$LOGFILE_DEFAULT; fi
 }
 
@@ -77,12 +87,12 @@ function testdependencies () {
   local doexit=0
   if ! command -v ruby &> /dev/null
   then
-      echo -e "\e[31m# Error: Command ruby could not be found. This is needed for s-post. Please install it, e.g.: apt-get install -y --no-install-recommends ruby-full\e[0m";
+      echo -e "\e[31m# Error: Command ruby could not be found. This is needed for s-post. Please install it, e.g.: \e[0mapt-get update; apt-get install -y --no-install-recommends ruby-full";
       doexit=1;
   fi
   if ! command -v sed &> /dev/null
   then
-      echo -e "\e[31m# Error: Command sed could not be found. This is needed for regular expressions. Please install it, e.g.: apt-get install -y --no-install-recommends sed\e[0m";
+      echo -e "\e[31m# Error: Command sed could not be found. This is needed for regular expressions. Please install it, e.g.: \e[0mapt-get update; apt-get install -y --no-install-recommends sed";
       doexit=1;
   fi
   if ! command -v zcat &> /dev/null
@@ -167,13 +177,15 @@ function usage() {
   echo -e "# Note that you cannot overwrite data with s-post, you have to delete them before." 1>&2; 
   echo -e "# Usage: \e[32m${0##*/}\e[0m [-s 'SNSB_import_[3-5]_*.normalized*.trig.gz']" 1>&2; 
   echo    "#   -h  ................................................ show this help usage" 1>&2; 
-  echo -e "#   -d  \e[32m'data.nhm.ac.uk'\e[0m ............................... domainname of this harvest (default: $DOMAINNAME)" 1>&2; 
+  echo -e "#   -d  \e[32m'CETAF-IDs-test-graphs'\e[0m ............. data set to import to (default: $SPARQL_END_POINT_DATASET)" 1>&2; 
+  echo -e "#   -u  \e[32m'data.nhm.ac.uk'\e[0m .................... URN domainname of this harvest (default: $URN_DOMAINNAME)" 1>&2; 
+  echo -e "#   -g  \e[32m'Paris_MNHN'\e[0m ........................ optional graph name to import into (by standard it is imported to the default graph)" 1>&2; 
   echo -e "#   -s  \e[32m'SNSB_import_*file-search-pattern*.trig.gz'\e[0m .... optional specific search pattern" 1>&2; 
   echo -e "#       Note: better use quotes for pattern with asterisk '*pattern*'" 1>&2; 
   echo -e "#       (default: '${FILE_SEARCH_PATTERN_DEFAULT}')" 1>&2; 
-  echo -e "#   -l \e[32m'special_logfile_20201101.log'\e[0m .................. logfile output" 1>&2; 
+  echo -e "#   -l \e[32m'special_logfile_20201101.log'\e[0m ....... logfile output" 1>&2; 
   echo -e "#       (default: $LOGFILE_DEFAULT)" 1>&2; 
-  echo -e "#   -w \e[32m'/import-data/rdf/tmpimport-kew'\e[0m ................ working directory" 1>&2; 
+  echo -e "#   -w \e[32m'/import-data/rdf/tmpimport-kew'\e[0m ..... working directory" 1>&2; 
   echo -e "#       (default: $THIS_WD)" 1>&2; 
   testdependencies;
   exit 1; 
@@ -188,9 +200,11 @@ function processinfo () {
   echo     "# * to convert RDF use convertRDF4import_normal-files.sh "
   echo     "# * manully delete old data (you cannot overwrite data with s-post on import)" 1>&2; 
   echo     "# Now ..."
-  echo     "# * we can use compressed *.trig.gz or uncompressed files for import via s-post"
-  echo  -e "# * we import to SPARQL end point http://localhost:3030/\e[32m${sparql_end_point}\e[0m"
+  echo  -e "# * we import via \e[34mruby s-post\e[0m (we can use compressed *.trig.gz or uncompressed files for it)"
+  echo  -e "# * we import to SPARQL end point http://localhost:3030/\e[32m${SPARQL_END_POINT_DATASET}\e[0m"
   echo  -e "# Read directory:  \e[32m${THIS_WD}\e[0m ..."
+  echo  -e "# Import GRAPH:    \e[32m${GRAPH}\e[0m ..."
+  echo  -e "# Data set:        \e[32m${SPARQL_END_POINT_DATASET}\e[0m ..."
   echo  -e "# Log file:        \e[32m/import-data/${LOGFILE}\e[0m"
   testdependencies;
 
@@ -211,13 +225,19 @@ function processinfo () {
   fi
 }
 
-while getopts "hd:l:s:w:" this_opt; do
+if [[ ${#} -eq 0 ]]; then
+    usage; exit 0;
+fi
+
+while getopts "hd:g:l:s:u:w:" this_opt; do
     case "${this_opt}" in
         d)
-            DOMAINNAME_OPTION=${OPTARG}
-            DOMAINNAME=$DOMAINNAME_OPTION;
-            # echo "# DEBUG getopts … case d: $DOMAINNAME"
-            if   [[ -z ${DOMAINNAME// /} ]] ; then echo "error: $DOMAINNAME cannot be empty" >&2; usage; exit 1; fi
+            SPARQL_END_POINT_DATASET=${OPTARG}
+            if   [[ -z ${SPARQL_END_POINT_DATASET// /} ]] ; then SPARQL_END_POINT_DATASET=CETAF-IDs ; fi
+            ;;
+        g)
+            GRAPH=${OPTARG}
+            if   [[ -z ${GRAPH// /} ]] ; then GRAPH=default ; fi
             ;;
         h)
             usage; exit 0;
@@ -229,6 +249,13 @@ while getopts "hd:l:s:w:" this_opt; do
         s)
             FILE_SEARCH_PATTERN="${OPTARG}"
             if   [[ -z ${FILE_SEARCH_PATTERN// /} ]] ; then file_search_pattern_default; FILE_SEARCH_PATTERN="$FILE_SEARCH_PATTERN_DEFAULT" ; fi
+            ;;
+        u)
+            # URN_DOMAINNAME_OPTION=${OPTARG}
+            URN_DOMAINNAME=${OPTARG};
+            # echo "# DEBUG getopts … case d: $URN_DOMAINNAME"
+            reset_logfile_name;
+            if   [[ -z ${URN_DOMAINNAME// /} ]] ; then echo "error: $URN_DOMAINNAME cannot be empty" >&2; usage; exit 1; fi
             ;;
         w)
             THIS_WD=${OPTARG}
@@ -274,28 +301,43 @@ esac
   for importDataFile in `if [[ -z "${FILE_SEARCH_PATTERN_NOT// /}" ]];then find "${THIS_WD}" -maxdepth 1 -type f -iname "${FILE_SEARCH_PATTERN##*/}" | sort --version-sort; else find "${THIS_WD}" -maxdepth 1 -type f -iname "${FILE_SEARCH_PATTERN##*/}" -and -not -iname "${FILE_SEARCH_PATTERN_NOT##*/}" | sort --version-sort;fi`; do
     # echo "# s-post ${importDataFile} ..."
     echo     "#----------------------------------------"
-    printf   "# Import via s-post (%04d of %04d) from %s ...\n"  $i $n "${importDataFile}" ; 
-    if [[ `echo $FILE_SEARCH_PATTERN | grep -i '\.gz$'` ]]; then 
+    printf   "# Import to GRAPH %s via s-post (%04d of %04d) from %s ...\n" $GRAPH $i $n "${importDataFile}" ; 
+    if [[ `echo $FILE_SEARCH_PATTERN | grep -i '\.gz$'` ]]; then
+      # 2022-04-28 s-post on its own cannot handle *.gz
       importDataFile_nonGzip=`echo "${importDataFile}" | sed --regexp-extended 's@(.+)\.gz@\1@'`
       importDataFile_temp="${importDataFile_nonGzip}_` date '+%Y-%m-%d_%Hh%Mm%Ss' `.${importDataFile_nonGzip##*.}"
-      echo "#   write temporary file  ${importDataFile_temp} ... "
+      echo "#   extract temporary file ${importDataFile_temp##/*/} ... "
       zcat "${importDataFile}" > "${importDataFile_temp}"
       # echo "ruby /jena-fuseki/bin/s-post --verbose http://localhost:3030/CETAF-IDs default ${importDataFile_temp} >> /import-data/${LOGFILE}"
-      ruby /jena-fuseki/bin/s-post --verbose http://localhost:3030/${sparql_end_point} default "${importDataFile_temp}" &>> "/import-data/${LOGFILE}"
-      echo "#   remove temporary file ${importDataFile_temp} ... "
+      # ruby /jena-fuseki/bin/s-post --verbose http://localhost:3030/${SPARQL_END_POINT_DATASET} default "${importDataFile_temp}" &>> "/import-data/${LOGFILE}"
+      echo "#   s-post ... "
+      ruby /jena-fuseki/bin/s-post --verbose http://localhost:3030/${SPARQL_END_POINT_DATASET} $GRAPH "${importDataFile_temp}" &>> "/import-data/${LOGFILE}"
+      echo "#   remove temporary file  ${importDataFile_temp##/*/} ... "
       rm "${importDataFile_temp}"
     else 
       # echo "# ruby /jena-fuseki/bin/s-post --verbose http://localhost:3030/CETAF-IDs default ${importDataFile} >> /import-data/${LOGFILE}"
-      ruby /jena-fuseki/bin/s-post --verbose http://localhost:3030/${sparql_end_point} default "${importDataFile}" &>> "/import-data/${LOGFILE}"
+      # ruby /jena-fuseki/bin/s-post --verbose http://localhost:3030/${SPARQL_END_POINT_DATASET} default "${importDataFile}" &>> "/import-data/${LOGFILE}"
+      echo "#   s-post ... "
+      ruby /jena-fuseki/bin/s-post --verbose http://localhost:3030/${SPARQL_END_POINT_DATASET} $GRAPH "${importDataFile}" &>> "/import-data/${LOGFILE}"
     fi
     i=$((i + 1 ))
   done
 
 n_failed_files=`cat "/import-data/${LOGFILE}" | grep "Failed" | wc -l `
+n_created_files=`cat "/import-data/${LOGFILE}" | grep "201 Created" | wc -l `
 n_ok_files=`cat "/import-data/${LOGFILE}" | grep "200 OK" | wc -l `
 echo     "#----------------------------------------"
 echo     "# Done. Check the import log file (if status is «200 OK», then it is fine)"
-printf   "# %02d files report status “\e[32m200 OK\e[0m” (details see in log file below)\n" $n_ok_files
+
+if [[ $n_created_files -gt 0 ]];then
+printf   "# \e[32m%02d\e[0m files report status “\e[32m201 Created\e[0m”\n" $n_created_files
+fi
+
+if [[ $n_ok_files -eq 0 ]];then
+printf   "# \e[33m%02d\e[0m files report status “\e[32m200 OK\e[0m” (something \e[33mis not right\e[0m; better check the logfile)\n" $n_ok_files
+else
+printf   "# \e[32m%02d\e[0m files report status “\e[32m200 OK\e[0m” (details see in log file below)\n" $n_ok_files
+fi
 if [[ $n_failed_files -gt 0 ]];then
 printf   "# %02d files report status “\e[33mFailed\e[0m” (details see in log file below)\n" $n_failed_files
 printf   "# %02d failed files can be checked by:\n#  grep --before-context=1 Failed /import-data/${LOGFILE}\n" $n_failed_files
