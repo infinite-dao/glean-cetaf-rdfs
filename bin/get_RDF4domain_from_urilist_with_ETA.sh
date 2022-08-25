25c11982da55a7356828cf29c9dad7fed95d4379
Andreas Plank <andreas.plank@web.de>
2022-08-25 13:56:26 +0200

    Update get_RDF4domain_from_urilist_with_ETA.sh
    set -eu (e -- exit immediately when a command fails; u -- exit on unset variables)

--- bin/get_RDF4domain_from_urilist_with_ETA.sh
+++ bin/get_RDF4domain_from_urilist_with_ETA.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+set -eu
+  # https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
+  # set -e -- option will cause a bash script to exit immediately when a command fails
+  # set -o -- exit also on non-existing command, print also BASH settings
+  # set -u -- this option causes the bash shell to treat unset variables as an error and exit immediately.
+  # set -x -- the -x option causes bash to print each command before executing it. This can be a great help when trying to debug a bash script failure. Note that arguments get expanded before a command gets printed, which will cause our logs to contain the actual argument values that were present at the time of execution!
+  # set -E -- traps are pieces of code that fire when a bash script catches certain signals. Aside from the usual signals (e.g. SIGINT, SIGTERM, …), traps can also be used to catch special bash signals like EXIT, DEBUG, RETURN, and ERR. However, reader Kevin Gibbs pointed out that using -e without -E will cause an ERR trap to not fire in certain scenarios.
 # Usage: download RDF files in parallel based on a text urilistfile and compress eventually harvested RDFs
 #   get_RDF4domain_from_urilist.sh -h # get help; see also function usage()
 # dependency: parallel
@@ -181,7 +188,7 @@ while getopts "d:h:j:u:lrt" o; do
             ;;
     esac
 done
-shift $((OPTIND-1))
+shift "$((OPTIND-1))"
 
 # TODO check zero padding of Thread-01_
 # $(printf "%.6d" "$myvar")
